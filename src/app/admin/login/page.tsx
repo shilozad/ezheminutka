08c044c67fb94a477aba249a77646292c0ef5237
shilozad <shilozad@ya.ru>
2026-08-11 18:18:59 +0300

    Исправить редирект авторизованного администратора

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -3,9 +3,11 @@ import { getAdminContext } from "@/lib/admin-auth";
 import AdminLogin from "@/components/admin/AdminLogin";
 export const dynamic = "force-dynamic";
 export default async function Page() {
+  let admin = null;
   try {
-    if (await getAdminContext()) redirect("/admin");
+    admin = await getAdminContext();
   } catch {}
+  if (admin) redirect("/admin");
   return (
     <div className="admin-login-page">
       <AdminLogin />
